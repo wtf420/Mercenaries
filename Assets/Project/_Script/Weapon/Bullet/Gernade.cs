2907dc32eebbfb0d1fc5b68126b056ab84410560
Nguyen Duy Minh Tuan <tgs.joker@gmail.com>
2023-10-07 14:54:18 +0700

    Gernade Now Explode and do stuff

--- Assets/Project/_Script/Weapon/Bullet/Gernade.cs
+++ Assets/Project/_Script/Weapon/Bullet/Gernade.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class Gernade : MonoBehaviour
 {
-    public float timer;
+    public float collisiontimer, explosionTimer, explosionRadius, damage;
     GameObject player;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Physics.IgnoreCollision(this.GetComponent<Collider>(), player.GetComponent<Collider>());
+        StartCoroutine(ignoreCollision());
+    }
+
+    IEnumerator ignoreCollision()
+    {
+        yield return new WaitForSeconds(collisiontimer);
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
     }
 
     void Update()
     {
-        if (timer > 0)
+        if (explosionTimer > 0)
         {
-            timer -= Time.deltaTime;
+            explosionTimer -= Time.deltaTime;
         } else
         {
-            Physics.IgnoreCollision(this.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+            Explode();
         }
+    }
+
+    void Explode()
+    {
+        RaycastHit[] info = Physics.SphereCastAll(this.transform.position, explosionRadius, Vector3.up, explosionRadius);
+        foreach (RaycastHit hit in info)
+        {
+            if (hit.collider.gameObject.GetComponent<Enemy>())
+            {
+                hit.collider.gameObject.GetComponent<Enemy>().TakenDamage(damage);
+            }
+        }
+        Destroy(gameObject);
     }
 }
 
